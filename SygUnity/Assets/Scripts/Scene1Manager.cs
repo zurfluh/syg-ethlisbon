d631c0316259d4fab27c10a2ad246112451f5509
Juan Martin Corallo <jmcorallo@gmail.com>
2021-10-23 13:20:24 +0100

    add logic to select planets with aura (#7)

--- SygUnity/Assets/Scripts/Scene1Manager.cs
+++ SygUnity/Assets/Scripts/Scene1Manager.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Scene1Manager : MonoBehaviour
 {
-    public GameObject menuPrefab;
-
-    // Start is called before the first frame update
-    void Start()
+    public Text uiText;
+    public GameObject selected;
+    private string selectedName;
+    public Vector3[] sizes;
+    
+    void Awake()
     {
-        
+        selectedName = "";
+        sizes = new Vector3[10];
+        for (int i = 0; i < sizes.Length; i++)
+        {
+            float f = 0.25f + i * 0.07f;
+            sizes[i] = new Vector3(f, f, f);
+        }
     }
 
     void Update()
@@ -19,9 +28,29 @@ public class Scene1Manager : MonoBehaviour
             Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
             
             RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-            if (hit.collider != null) {
-                Debug.Log(hit.collider.gameObject.name);
-                Instantiate(menuPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            if (hit.collider != null && hit.collider.gameObject.name != selectedName) {
+                if (selected != null) {
+                    selected.transform.GetChild(0).gameObject.SetActive(false);
+                }
+                selected = hit.collider.gameObject;
+                selectedName = selected.name;
+                uiText.text = selectedName;
+                Transform child = selected.transform.GetChild(0);
+                child.gameObject.SetActive(true);
+                PlanetManager pm = selected.gameObject.GetComponent<PlanetManager>();
+                float eth = pm.getStakedEth();
+
+                // Size
+                int index = Mathf.FloorToInt(eth);
+                child.transform.localScale = sizes[index];
+
+                // Color
+                SpriteRenderer sr = child.gameObject.GetComponent<SpriteRenderer>();
+                float r = 0.3f + 0.03f * index;
+                float g = 0.1f * index;
+                float b = 0.2f + 0.06f * index;
+                Color color = new Color(r, g, b);
+                sr.color = color;
             }
         }
     }
