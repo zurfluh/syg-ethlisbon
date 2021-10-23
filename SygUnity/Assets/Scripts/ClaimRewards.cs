13e2a603d84a1067f7ca05f2f5de09562b619a6e
Stefan Zurfluh <stefan.zurfluh@sygnum.com>
2021-10-23 17:11:31 +0100

    Prepare function call for external signing

--- SygUnity/Assets/Scripts/ClaimRewards.cs
+++ SygUnity/Assets/Scripts/ClaimRewards.cs
@@ -2,6 +2,8 @@
 using UnityEngine.UI;
 using Nethereum.Web3;
 using SygEthlisbon.Contracts.SpaceMafia;
+using SygEthlisbon.Contracts.SpaceMafia.ContractDefinition;
+using Nethereum.RPC.Eth.DTOs;
 
 public class ClaimRewards : MonoBehaviour
 {  
@@ -17,10 +19,24 @@ public class ClaimRewards : MonoBehaviour
     public async void CallSomeFunction()
     {
         var web3 = new Web3(GameManager.Instance.InfuraUrl);
+
         var spaceMafiaService = new SpaceMafiaService(web3, GameManager.Instance.SpaceMafiaContractAddress);
 
-        // TODO: Token ID as input.
-        var txHash = await spaceMafiaService.ClaimDividendsRequestAsync(123456);
-        Debug.Log("Transaction hash for function call: " + txHash);
+        var claimDividendsFunction = spaceMafiaService.ContractHandler.GetFunction<ClaimDividendsFunction>();
+
+        var currentNonce = await web3.Eth.Transactions.GetTransactionCount.SendRequestAsync("0x54782", BlockParameter.CreatePending());
+        Debug.Log("Current nonce: " + currentNonce);
+        
+        var transactionInput = claimDividendsFunction.CreateTransactionInput(new ClaimDividendsFunction
+        {
+            Nonce = currentNonce.Value + 1,
+            TokenId = 45678
+        }, "0x123456765434567");
+
+        Debug.Log("Transaction input: " + transactionInput);
+
+        string rawTx = transactionInput.Data;
+        Debug.Log("Raw transaction: " + rawTx);
+        // TODO: Sign and send trx by external wallet.
     }
 }
