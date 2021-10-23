e47fededed67762d057a59f150fd551fc5c419fc
Samuele Rodi <samuele.rodi@sygnum.com>
2021-10-23 18:45:50 +0100

    commented pools

--- smartcontract/deploy/1_migrations.ts
+++ smartcontract/deploy/1_migrations.ts
@@ -2,24 +2,24 @@ const hre = require("hardhat");
 module.exports = async({getNamedAccounts, deployments}) => {
     const accounts = await getNamedAccounts()
     const GalaxyToken = await hre.ethers.getContractFactory("GalaxyToken");
-    const galaxyToken = await GalaxyToken.deploy({gasLimit: 80000000});
+    const galaxyToken = await GalaxyToken.deploy({gasLimit: 12450000});
     console.log("galaxyToken deployed to: ", galaxyToken.address);
     const SpaceMafia = await hre.ethers.getContractFactory("SpaceMafia");
-    const spaceMafia = await SpaceMafia.deploy({gasLimit: 80000000});
+    const spaceMafia = await SpaceMafia.deploy({gasLimit: 12450000});
     await spaceMafia.deployed();
     console.log("spaceMafia deployed to:", spaceMafia.address);
-    await galaxyToken.transferOwnership(spaceMafia.address);
+    await galaxyToken.transferOwnership(spaceMafia.address, {gasLimit: 12450000});
 
-    await spaceMafia.initialize(galaxyToken.address, {gasLimit: 8000000});
+    await spaceMafia.initialize(galaxyToken.address, {gasLimit: 12450000});
 
     const planetType = await spaceMafia.planetType();
     console.log('planetType: ', planetType);
 
     // MINT PLANETS
-    await galaxyToken.nonFungibleMint("0xbf652059f7fE27e4e39dBD3B5B5E5eAbC34c6741",planetType,'')
-    await galaxyToken.nonFungibleMint("0xbf652059f7fE27e4e39dBD3B5B5E5eAbC34c6741",planetType,'')
-    await galaxyToken.nonFungibleMint("0xbf652059f7fE27e4e39dBD3B5B5E5eAbC34c6741",planetType,'')
-    await galaxyToken.nonFungibleMint("0xbf652059f7fE27e4e39dBD3B5B5E5eAbC34c6741",planetType,'')
-    await galaxyToken.nonFungibleMint("0xbf652059f7fE27e4e39dBD3B5B5E5eAbC34c6741",planetType,'')
+    await spaceMafia.mintPlanet("0xbf652059f7fE27e4e39dBD3B5B5E5eAbC34c6741",'', {gasLimit: 12450000})
+    await spaceMafia.mintPlanet("0xbf652059f7fE27e4e39dBD3B5B5E5eAbC34c6741",'', {gasLimit: 12450000})
+    await spaceMafia.mintPlanet("0xbf652059f7fE27e4e39dBD3B5B5E5eAbC34c6741",'', {gasLimit: 12450000})
+    await spaceMafia.mintPlanet("0xbf652059f7fE27e4e39dBD3B5B5E5eAbC34c6741",'', {gasLimit: 12450000})
+    await spaceMafia.mintPlanet("0xbf652059f7fE27e4e39dBD3B5B5E5eAbC34c6741",'', {gasLimit: 12450000})
   };
   module.exports.tags = ['All'];
